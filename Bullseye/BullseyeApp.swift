6d506233dd4bc150c70d965c243cc54c49aa2b9c
Azat <kaumov.a.r@gmail.com>
2021-04-15 21:08:26 +0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Azat Kaiumov on 15.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
