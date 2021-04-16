3cae713c87c85ea63b2957ce62e4b8079bd2b9fa
Azat <kaumov.a.r@gmail.com>
2021-04-16 03:13:37 +0300

    Add views

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN")
+            Text("89")
+            HStack {
+                Text("1")
+                Slider(value: .constant(50), in: 1.00...100.00)
+                Text("2")
+            }
+            Button(action: {}) {
+                Text("Hit me")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
