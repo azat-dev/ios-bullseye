8c86a44069c9a032a5b7ca1dc80d9c3ac7bce560
Azat <kaumov.a.r@gmail.com>
2021-04-17 13:25:19 +0300

    Add dialog with current value

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,18 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isAlertOpened: Bool = false
+    @State var currentValue: Double = 50.0
+    
+    
     var body: some View {
         VStack {
             Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
             Text("89")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .kerning(-1.0)
             HStack {
                 Text("1")
-                Slider(value: .constant(50), in: 1.00...100.00)
+                    .font(.body)
+                    .fontWeight(.bold)
+                Slider(value: self.$currentValue, in: 1.00...100.00)
                 Text("2")
+                    .font(.body)
+                    .fontWeight(.bold)
             }
-            Button(action: {}) {
+            Button(action: {
+                self.isAlertOpened = true
+            }) {
                 Text("Hit me")
-            }
+            }.alert(isPresented: $isAlertOpened, content: {
+                let roundedValue = Int(self.currentValue)
+               return Alert(
+                    title: Text("Score"),
+                message: Text("The slider's value is \(self.currentValue) rounded \(self.currentValue.rounded())"),
+                    dismissButton: .default(Text("Awesome!"))
+               )
+            })
         }
     }
 }
@@ -27,6 +52,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+            
         ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
