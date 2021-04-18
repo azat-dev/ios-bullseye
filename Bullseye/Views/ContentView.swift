c029e176a2ce50ba46df3e9dd1df5d6a183856e2
Azat <kaumov.a.r@gmail.com>
2021-04-18 13:14:23 +0300

    Style views

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -14,43 +14,78 @@ struct ContentView: View {
     
     
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN")
-                .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-            Text("\(game.target)")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .kerning(-1.0)
-            HStack {
-                Text("1")
-                    .font(.body)
-                    .fontWeight(.bold)
-                Slider(value: $currentValue, in: 1.00...100.00)
-                Text("100")
-                    .font(.body)
-                    .fontWeight(.bold)
-            }
-            Button(action: {
-                isAlertOpened = true
-            }) {
-                Text("Hit me")
-            }.alert(isPresented: $isAlertOpened, content: {
-                let value = currentValue.rounded()
-                let earnedPoints = game.points(value: Int(value))
-               
-                let valueText = "The slider's value is \(value)"
-                let pointsText = "You've earned \(earnedPoints)"
+        ZStack {
+            Color("BackgroundColor").edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("🎯🎯🎯\nPut the Bullsyee as close as you can".uppercased())
+                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                    .foregroundColor(Color("TextColor"))
                 
-                return Alert(
-                    title: Text("Score"),
-                message: Text("\(valueText)\n\(pointsText)"),
-                    dismissButton: .default(Text("Awesome!"))
-               )
-            })
+                Text("\(game.target)")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .kerning(-1.0)
+                    .foregroundColor(Color("TextColor"))
+                
+                HStack {
+                    Text("1")
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("TextColor"))
+                    
+                    Slider(value: $currentValue, in: 1.00...100.00)
+                    
+                    Text("100")
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("TextColor"))
+                }
+                Button(action: {
+                    isAlertOpened = true
+                }) {
+                    Text("hit me".uppercased())
+                        .fontWeight(.bold)
+                        .font(.title3)
+                        .foregroundColor(Color.white)
+                }
+                .padding(20)
+                .background(ZStack {
+                    Color("ButtonColor")
+                    LinearGradient(
+                        gradient: Gradient(
+                            colors: [
+                                Color(
+                                    red: 1,
+                                    green: 1,
+                                    blue: 1,
+                                    opacity: 0.3
+                                ),
+                                Color.clear
+                            ]),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                })
+                .cornerRadius(21.0)
+
+                .alert(isPresented: $isAlertOpened, content: {
+                    let value = currentValue.rounded()
+                    let earnedPoints = game.points(value: Int(value))
+
+                    let valueText = "The slider's value is \(value)"
+                    let pointsText = "You've earned \(earnedPoints)"
+
+                    return Alert(
+                        title: Text("Score"),
+                    message: Text("\(valueText)\n\(pointsText)"),
+                        dismissButton: .default(Text("Awesome!"))
+                   )
+                })
+            }.padding(20.0)
         }
     }
 }
@@ -59,5 +94,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         ContentView().previewLayout(.fixed(width: 568, height: 320))
+    
+        ContentView()
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     }
 }
